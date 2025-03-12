27cec86aa479984e3de496663918e0436ee07709
zahmad-avenues <zidan_ahmad@avenues-ny.org>
2025-03-12 19:39:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,50 @@
-// Information about the data:
-// https://github.com/fivethirtyeight/data/tree/master/us-weather-history
-
-const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv"
-
-// https://p5js.org/reference/p5/p5.Table/
-let weatherTable
-
-let currentRow = 0
-let currentColumn = 1
-
+let weatherJSON 
+let minPrecip = Infinity
+let maxPrecip = -Infinity
+let images = {}
+let dx
 function preload() {
-  weatherTable = loadTable(weatherURL, 'csv', 'header')
+  weatherJSON = loadJSON("https://api.weather.gov/gridpoints/OKX/33,37/forecast")  
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  dx = width/(weatherJSON.properties.periods.length+2)
+  
+  // const fit = (img) => {
+  //   img.resize(dx,0)
+  //   redraw()
+  // }
+  
+  for( const p of weatherJSON.properties.periods ) {
+    minPrecip =min(p.probabilityOfPrecipitation.value, minPrecip)
+    maxPrecip = max(p.probabilityOfPrecipitation.value, maxPrecip)
+    // if( ! (p.icon in images) ) {
+    //   images[p.icon] = loadImage(p.icon, fit) 
+    // }    
+  }
   noLoop()
-  textAlign(CENTER, CENTER)
-  // console.log(weatherTable.getRowCount())
 }
 
 function draw() {
   background(220);
-  const temp = weatherTable.getNum(currentRow,currentColumn)
-  text(temp,width/2,height/2)
+  let px = dx // dx * (i+1), where i = 0
+  let py = map( weatherJSON.properties.periods[0].probabilityOfPrecipitation.value, minPrecip, maxPrecip, 0.8*height, 0.2*height)
+  for( let i = 1; i < weatherJSON.properties.periods.length; i++ ) {
+    let cx = dx * (i+1)
+    let cy = map( weatherJSON.properties.periods[i].probabilityOfPrecipitation.value, minPrecip, maxPrecip, 0.8*height, 0.2*height)
+    line(px,py,cx,cy)
+    px = cx
+    py = cy
+    // if( weatherJSON.properties.periods[i].icon in images ) {
+    //   image(images[weatherJSON.properties.periods[i].icon],dx*(i+1),height/2)   
+    // }    
+  }
+  text("Days",width/2,height-10)
+  text("Probability of Rain",10, height/2)
+  // console.log(images)
 }
 
 function keyPressed() {
-  if( keyCode === DOWN_ARROW ) {
-    currentRow++
-    redraw()
-  }
-  if( keyCode === RIGHT_ARROW ) {
-    currentColumn++
-    if( currentColumn >= weatherTable.getColumnCount() ) {
-      currentColumn = 1
-    }
-    redraw()
-  }
+  redraw()
 }
